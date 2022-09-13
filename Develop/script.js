e719e9904b67aa136d10365940eff498fa781ed2
Xiaojing <8redfishcreative@gmail.com>
2022-09-13 09:22:29 -0400

    add time-block

--- Develop/script.js
+++ Develop/script.js
@@ -2,8 +2,11 @@
  var currentDay = $('#currentDay');
 
 //display current day
-var d = new Date();
-newDay = moment(d).format('dddd, MMMM Do');  
+//var d = new Date();
+newDay = moment().format('dddd, MMMM Do');  
 currentDay.text(newDay);
+
+//display scroll down
+$('.container').tooltip({ boundary: 'window' })
 
 
