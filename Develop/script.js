653fb08e84add35cc62b3ee333c8678224313608
Xiaojing <8redfishcreative@gmail.com>
2022-09-13 19:42:17 -0400

    timeTracker function

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,36 @@
 
  var currentDay = $('#currentDay');
-
+var lists = $('.list-group');
 //display current day
 //var d = new Date();
 newDay = moment().format('dddd, MMMM Do');  
 currentDay.text(newDay);
 
-//display scroll down
-$('.container').tooltip({ boundary: 'window' })
+//display different color on each list
+function timeTracker(){
+    var currentHour = moment().hour();
+    console.log(currentHour);
+        
+    $( ".row" ).each(function() {
+    var listTime = parseInt($(this).attr("id").split("hour")[1]); 
+        console.log(listTime);
+    var textArea = $('.textArea');
+    console.log(textArea);
+        if(listTime < currentHour){
+        textArea.removeClass("future");
+        textArea.removeClass("present");
+        textArea.addClass("past");
 
+        }else if(listTime > currentHour){
+        textArea.removeClass("past");
+        textArea.removeClass("present");
+        textArea.addClass("future");
 
+        }else{
+        textArea.removeClass("past");
+        textArea.removeClass("future");
+        textArea.addClass("present");
+        }
+    })
+}
+timeTracker();
