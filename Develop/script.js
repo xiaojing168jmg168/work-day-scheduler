41143788ceeabc36097f4b9ec777d1f46e6e4c2c
Xiaojing <8redfishcreative@gmail.com>
2022-09-14 10:47:40 -0400

    add set local save,get local save, add show added and fade out added

--- Develop/script.js
+++ Develop/script.js
@@ -1,36 +1,80 @@
 
- var currentDay = $('#currentDay');
+var currentDay = $('#currentDay');
 var lists = $('.list-group');
+
+var saveBtn = $('.saveBtn');
+var newDiv;
+
+$(document).ready(function () {// tells engine to load 1)html & 2)css first.
+   
 //display current day
-//var d = new Date();
 newDay = moment().format('dddd, MMMM Do');  
 currentDay.text(newDay);
+
+ // saveBtn click listener 
+$(".saveBtn").on("click", function () {
+        // Get nearby values of the textArea in JQuery
+        var text = $(this).siblings(".textArea").val();
+        var time = $(this).parent().attr("id");
+
+        // Save text in local storage
+        localStorage.setItem(time, text);
+        
+        //every savebtn remind added
+
+
+        var createDiv = $('<div class="showAdded">Appoinment Added to <span style="color:Tomato;">localstorage</span> ✅</div>')
+       
+        $(".container").prepend(createDiv); 
+
+        //after 3 seconds, the showAdded disappear
+        setTimeout(fade_out, 2000);
+
+        function fade_out() {
+        $(".showAdded").fadeOut().empty();
+        }
+
+      
+ })
+console.log(localStorage);
+
+
 
 //display different color on each list
 function timeTracker(){
     var currentHour = moment().hour();
     console.log(currentHour);
         
-    $( ".row" ).each(function() {
+    $( ".time-block" ).each(function() {
     var listTime = parseInt($(this).attr("id").split("hour")[1]); 
-        console.log(listTime);
-    var textArea = $('.textArea');
-    console.log(textArea);
-        if(listTime < currentHour){
-        textArea.removeClass("future");
-        textArea.removeClass("present");
-        textArea.addClass("past");
+     if(listTime < currentHour){
+        $(this).children('.textArea').removeClass("future");
+        $(this).children('.textArea').removeClass("present");
+        $(this).children('.textArea').addClass("past");
 
-        }else if(listTime > currentHour){
-        textArea.removeClass("past");
-        textArea.removeClass("present");
-        textArea.addClass("future");
+     }else if(listTime === currentHour){
+        $(this).children('.textArea').removeClass("past");
+        $(this).children('.textArea').removeClass("future");
+        $(this).children('.textArea').addClass("present");
+     }else{
 
-        }else{
-        textArea.removeClass("past");
-        textArea.removeClass("future");
-        textArea.addClass("present");
+        $(this).children('.textArea').removeClass("past");
+        $(this).children('.textArea').removeClass("present");
+        $(this).children('.textArea').addClass("future");
         }
     })
+
 }
-timeTracker();
+ timeTracker();
+
+// Get item from local storage 
+ $("#hour9 .textArea").val(localStorage.getItem("hour9"));
+ $("#hour10 .textArea").val(localStorage.getItem("hour10"));
+ $("#hour11 .textArea").val(localStorage.getItem("hour11"));
+ $("#hour12 .textArea").val(localStorage.getItem("hour12"));
+ $("#hour13 .textArea").val(localStorage.getItem("hour13"));
+ $("#hour14 .textArea").val(localStorage.getItem("hour14"));
+ $("#hour15 .textArea").val(localStorage.getItem("hour15"));
+ $("#hour16 .textArea").val(localStorage.getItem("hour16"));
+ $("#hour17 .textArea").val(localStorage.getItem("hour17"));
+})
